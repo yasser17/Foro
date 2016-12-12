9d22170ed783ba5d9d81bcab13e6fd8a802e49f2
Yasser <yasser@fx2.com.uy>
2016-12-12 08:23:39 -0300

    some changes

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -21,7 +21,7 @@ class CommentController extends Controller
     public function accept(Comment $comment)
     {
         $comment->markAsAnswer();
-        
+
         return redirect($comment->post->url);
     }
 }
